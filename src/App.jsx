a46097d08b048f2e4ee43368b0a71bab8b4fb0bf
moussasg <117439847+moussasg@users.noreply.github.com>
2023-09-27 20:38:53 +0100

    mytailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS styled component.
+    </div>
+  );
+}
+export default App;
